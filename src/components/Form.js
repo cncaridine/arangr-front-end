7346c26591562320f9beb135dcdd97f7b17a3f47
Brendan Quirk <brendanquirk@Brendans-MBP.home>
2019-03-02 18:37:17 -0500

    updated the original state of the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,14 +4,20 @@ class Form extends Component {
   constructor(props){
     super(props)
     this.state = {
-      event: '',
+      eventVar: '',
+      title: '',
+      image: '',
+      date: '',
+      time: 0,
+      location: '',
+      description: '',
       rsvp: false
     }
   }
 
   handleChange = (event) => {
     this.setState({
-      event: event.target.value
+      eventVar: event.target.value
     })
   }
 
@@ -31,11 +37,11 @@ class Form extends Component {
     return(
       <div className="form">
         <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="name" value={this.state.event.name} onChange={this.handleChange}/>
-          <input type="text" placeholder="location" value={this.state.event.location} onChange={this.handleChange}/>
-          <input type="text" placeholder="date" value={this.state.event.date} onChange={this.handleChange}/>
-          <input type="text" placeholder="time" value={this.state.event.time} onChange={this.handleChange}/>
-          <input type="text" placeholder="image" value={this.state.event.image} onChange={this.handleChange}/>
+          <input type="text" placeholder="title" value={this.state.title} onChange={this.handleChange}/>
+          <input type="text" placeholder="location" value={this.state.location} onChange={this.handleChange}/>
+          <input type="text" placeholder="date" value={this.state.date} onChange={this.handleChange}/>
+          <input type="text" placeholder="time" value={this.state.time} onChange={this.handleChange}/>
+          <input type="text" placeholder="image" value={this.state.image} onChange={this.handleChange}/>
           <button type="submit">Submit Event</button>
         </form>
       </div>
